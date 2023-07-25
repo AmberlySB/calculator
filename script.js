8d171f4d33e05632d7e89f55374231cec20ba696
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-25 18:15:39 -0500

    Implement operation function

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-console.log(division(40, 2))
+let a = 10;
+let b = 5;
+let op = '/';
+
+function operate (a, b, op) {
+    if (op === '+') result = add(a, b);
+    else if (op === '-') result = subtract(a, b);
+    else if (op === '*') result = multiply(a, b);
+    else if (op === '/') result = divide(a, b);
+    return result;
+}
+
+console.log(operate(a, b, op));
 
 function add(a, b) {
     return a + b;
@@ -12,6 +24,6 @@ function multiply(a, b) {
     return a * b;
 }
 
-function division(a, b) {
+function divide(a, b) {
     return a / b;
 }

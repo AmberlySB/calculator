b564f822580abc593a46e46988c272d65d61d6e1
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-26 06:01:35 -0500

    Implement display function

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', populateDisplay), false;
+});
+
 let a = 10;
 let b = 5;
-let op = "/";
+let op = '/';
 
 function operate(a, b, op) {
-  if (op === "+") result = add(a, b);
-  else if (op === "-") result = subtract(a, b);
-  else if (op === "*") result = multiply(a, b);
-  else if (op === "/") result = divide(a, b);
+  if (op === '+') result = add(a, b);
+  else if (op === '-') result = subtract(a, b);
+  else if (op === '*') result = multiply(a, b);
+  else if (op === '/') result = divide(a, b);
   return result;
 }
 
 console.log(operate(a, b, op));
+
+let displayValue;
+const digits = [];
+
+function populateDisplay() {
+  const display = document.getElementById('display');
+  if (
+    +this.textContent > -1 ||
+    +this.textContent < 10 ||
+    this.textContent === '.'
+  ) {
+    if (displayValue === undefined) {
+      console.log('undefined: ', typeof displayValue);
+      if (this.textContent === '.') {
+        displayValue = '0' + this.textContent;
+        digits.push('0');
+        console.log('digits: ', digits);
+      } else {
+        displayValue = this.textContent;
+        digits.push(this.textContent);
+        console.log('digits: ', digits);
+      }
+    } else if (digits.length < 9) {
+      const split = displayValue.split('');
+      const checkDot = split.filter((dot) => dot === '.');
+      if (checkDot.join('') === '.') {
+        console.log('dot status', checkDot.join(''));
+        if (this.textContent === '.') {
+          console.log('test', this.textContent);
+          displayValue = displayValue;
+        } else {
+          console.log('high mom');
+          displayValue = displayValue + this.textContent;
+          digits.push(this.textContent);
+          console.log('digits: ', digits);
+        }
+      } else if (this.textContent === '.') {
+        displayValue = displayValue + this.textContent;
+      } else {
+        console.log('hello world');
+        displayValue = displayValue + this.textContent;
+        digits.push(this.textContent);
+        console.log('digits-end: ', digits);
+      }
+    }
+    display.textContent = displayValue;
+    console.log(displayValue);
+  }
+}
 
 function add(a, b) {
   return a + b;

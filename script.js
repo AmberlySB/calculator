42704e9713122c7778bcc79e4338a4eea09256a1
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-25 23:35:06 -0500

    Formatting applied

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let a = 10;
 let b = 5;
-let op = '/';
+let op = "/";
 
-function operate (a, b, op) {
-    if (op === '+') result = add(a, b);
-    else if (op === '-') result = subtract(a, b);
-    else if (op === '*') result = multiply(a, b);
-    else if (op === '/') result = divide(a, b);
-    return result;
+function operate(a, b, op) {
+  if (op === "+") result = add(a, b);
+  else if (op === "-") result = subtract(a, b);
+  else if (op === "*") result = multiply(a, b);
+  else if (op === "/") result = divide(a, b);
+  return result;
 }
 
 console.log(operate(a, b, op));
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
